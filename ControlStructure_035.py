9259480f73f91cb9e8d938c76f6b67a4240a07c0
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-09-24 11:24:28 +0700

    menambahkan loop untuk mencetak pola bintang dengan perbaikan format output

--- ControlStructure_035.py
+++ ControlStructure_035.py
@@ -30,3 +30,10 @@ for i in range(n):
     for j in range(i + 1):
         print("*", end="")
     print()
+
+n = int(input("Masukkan n:"))
+
+for i in range(n):
+    for j in range(1,  n + 1):
+        print("*", end="")
+    print()
