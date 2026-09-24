5978e6371d32f2c2a6277d2cc042b6bccedd8b6a
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-09-24 11:06:06 +0700

    menambahkan program student performance evaluation based on input score

---
+++ ControlStructure_035.py
@@ -0,0 +1,11 @@
+nilai = input("Masukkan nilai siswa:")
+if nilai >= 90:
+    print("Excellent performance")
+elif nilai >= 80:
+    print("Very Good performance")
+elif nilai >= 70:
+    print("Good performance")
+elif nilai >= 60:
+    print("Average performance")  
+else:
+    print("Needs improvement")
